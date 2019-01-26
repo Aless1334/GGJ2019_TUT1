78302fa3609739b8931638936b1da3dede2f4c17
Aless1334 <aless3511@gmail.com>
2019-01-27 01:07:48 +0900

    baseTransform削除

--- GGJ2019_TUT1/Assets/Nagasono/Scripts/CompassMarker.cs
+++ GGJ2019_TUT1/Assets/Nagasono/Scripts/CompassMarker.cs
@@ -5,13 +5,12 @@ using UnityEngine;
 public class CompassMarker : MonoBehaviour
 {   
     [SerializeField] private Goal goalObject;
-    [SerializeField] private Transform baseTransform;
 
     // Update is called once per frame
     void Update()
     {
         if (goalObject == null) return;
-        var direction = (goalObject.transform.position - baseTransform.position).normalized;
+        var direction = (goalObject.transform.position - transform.position).normalized;
         var lookRotation = Quaternion.LookRotation(Vector3.back, direction);
         transform.localRotation = lookRotation;
     }
