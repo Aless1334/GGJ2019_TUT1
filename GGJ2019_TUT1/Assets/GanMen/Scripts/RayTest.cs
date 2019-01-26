3c02e359d2800f2a30815016329668241aa0eb2c
GanGanKamen <m01171408c@edu.teu.ac.jp>
2019-01-26 21:20:50 +0900

    Wolf_ver2を使って、StartChase()で追跡を始める

---
+++ GGJ2019_TUT1/Assets/GanMen/Scripts/RayTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayTest : MonoBehaviour
+{
+    public float x, y;
+    // Start is called before the first frame update
+    void Start()
+    {
+        x = 0;
+        y = transform.position.x + 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            RayShoot(x, y);
+        }
+    }
+
+    private void RayShoot(float x, float y)
+    {
+        Ray ray = new Ray(transform.position, new Vector3(x, y, 0));
+        int distance = 10;
+        RaycastHit hit;
+        Debug.DrawLine(ray.origin, ray.direction, Color.red);
+        if (Physics.Raycast(ray, out hit, distance))
+        {
+            Debug.Log("false");
+
+
+        }
+    }
+}
--- GGJ2019_TUT1/Assets/GanMen/Scripts/Wolf_ver2.cs
+++ GGJ2019_TUT1/Assets/GanMen/Scripts/Wolf_ver2.cs
@@ -9,8 +9,10 @@ public class Wolf_ver2 : MonoBehaviour
     private Collider2D wall;
     [SerializeField] private bool isOnCollision;
     public float speed;
-    private bool isChase;
-    private Rigidbody2D rb;
+    public bool isChase;
+    //public Rigidbody2D rb;
+    private bool canGo;
+    public float distance;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,13 +22,108 @@ public class Wolf_ver2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(isChase == true)
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            RayShoot(1, 0);
+        }
+        if (isChase == false)
         {
             transform.position = Vector3.Lerp(transform.position, chaseTarget, speed);
         }
     }
 
     public void StartChase()
+    {
+        StartCoroutine(ChaseAI());
+    }
+
+    private IEnumerator ChaseAI()
+    {
+        chaseTarget = player.transform.position;
+        while (isOnCollision == false)
+        {
+            yield return null;
+        }
+        Debug.Log("y");
+        if (Mathf.Abs(transform.position.y - wall.transform.position.y) < Mathf.Abs(transform.position.x - wall.transform.position.x))
+        {
+            if (player.transform.position.y < transform.position.y)
+            {
+                chaseTarget = new Vector3(transform.position.x, transform.position.y - distance, transform.position.z);
+            }
+            else
+            {
+                chaseTarget = new Vector3(transform.position.x, transform.position.y + distance, transform.position.z);
+            }
+        }
+        else
+        {
+            if(player.transform.position.x < transform.position.x)
+            {
+                chaseTarget = new Vector3(transform.position.x - distance, transform.position.y, transform.position.z);
+            }
+            else
+            {
+                chaseTarget = new Vector3(transform.position.x + distance, transform.position.y, transform.position.z);
+            }
+        }
+        
+        while (isOnCollision == true)
+        {
+            yield return null;
+        }
+        Debug.Log("x");
+        StartChase();
+
+
+    }
+    private void RayShoot(float x, float y)
+    {
+        Ray ray = new Ray(transform.position, new Vector3(x, y, 0));
+        int distance = 10;
+        RaycastHit hit;
+        Debug.DrawLine(ray.origin, ray.direction, Color.red);
+        if (Physics.Raycast(ray, out hit, distance))
+        {
+            Debug.Log("false");
+
+
+        }
+    }
+    private bool RayTest(float x, float y)
+    {
+        Ray ray = new Ray(transform.position, new Vector3(x, y, 0));
+        //new Vector3(transform.position.x * x, transform.position.y * y, transform.position.z));
+        RaycastHit hit;
+        Debug.DrawLine(ray.origin, ray.direction, Color.red);
+        if (Physics.Raycast(ray, out hit, distance))
+        {
+            Debug.Log("false");
+            return false;
+
+        }
+
+        else
+        {
+            return true;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        isOnCollision = true;
+        wall = collision;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+
+        isOnCollision = false;
+        //Invoke("AvoidReset", 1f);
+        wall = null;
+    }
+
+    private void PosChangeCheck()
     {
 
     }
