8dc3640ef96ae552721e57561bf0be6a6306b2ca
Aless1334 <aless3511@gmail.com>
2019-01-26 20:00:55 +0900

    アイテムデータベース作成

---
+++ GGJ2019_TUT1/Assets/Nagasono/Scripts/Settings/ItemDatabase.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "DataBase/Item", fileName = "New ItemDatabase")]
+public class ItemDatabase : ScriptableObject
+{
+    [SerializeField] private BaseItem[] items;
+
+    public BaseItem[] Items
+    {
+        get { return items; }
+    }
+}
