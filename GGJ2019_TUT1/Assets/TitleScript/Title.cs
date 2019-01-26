10a6b93f354d37299fe9a1c0690217b75c580f12
Hiraide75 <47030614+Hiraide75@users.noreply.github.com>
2019-01-26 18:48:07 +0900

    Add Titlescenes

---
+++ GGJ2019_TUT1/Assets/TitleScript/Title.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Title : MonoBehaviour
+{
+    [SerializeField, Header("操作説明画面")]
+    GameObject explane;
+    FadeManager fadeManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fadeManager = FindObjectOfType<FadeManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fadeManager != null && fadeManager.GetFade()) return;
+        if (explane == null || explane.activeInHierarchy)
+        {
+            if (Input.GetKeyDown(KeyCode.Return) && fadeManager != null)
+            {
+                fadeManager.SetFadeOut();
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.Return)) explane.SetActive(true);
+
+    }
+}
