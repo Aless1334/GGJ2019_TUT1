a9c7064cbbca38e1d74bbd0191dbbdaf0662c5d6
Aless1334 <aless3511@gmail.com>
2019-01-26 19:30:15 +0900

    アイテムプレハブ作成

---
+++ GGJ2019_TUT1/Assets/Nagasono/Scripts/ScoreItem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Nagasono.AudioScripts;
+using UnityEngine;
+
+public class ScoreItem : BaseItem
+{
+    [SerializeField] private int score;
+    
+    public override void Get()
+    {
+        Destroy(gameObject);
+        GimmickScore.Instance.Add(score);
+        AudioManager.PlayAudio("ItemPick");
+    }
+
+    public override void Use()
+    {
+        throw new System.NotImplementedException();
+    }
+}
