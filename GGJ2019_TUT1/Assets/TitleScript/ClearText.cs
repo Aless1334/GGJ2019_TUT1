ad1191e62ee36fc805a4edc99455c7fbffd73c42
Hiraide75 <47030614+Hiraide75@users.noreply.github.com>
2019-01-26 20:10:32 +0900

    Add EndScene

---
+++ GGJ2019_TUT1/Assets/TitleScript/ClearText.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClearText : MonoBehaviour
+{
+    float getScore; //渡してもらうスコア
+    public float MaxScore;
+    Text myText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        getScore = GimmickScore.Instance.GetPoint();
+        float result;
+        result = getScore / MaxScore * 100;   //本来の計算処理
+        //result = 100;   //仮置きの数字
+        myText = GetComponentInChildren<Text>();
+        myText.text = "Score : " + result + "%";
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
