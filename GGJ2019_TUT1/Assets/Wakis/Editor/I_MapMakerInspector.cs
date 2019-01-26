c97b805209b95546241371b36c3f2840b1c39cfc
Aless1334 <aless3511@gmail.com>
2019-01-26 21:19:15 +0900

    画像リサイズ

---
+++ GGJ2019_TUT1/Assets/Wakis/Editor/I_MapMakerInspector.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(I_MapMaker))]
+public class I_MapMakerInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        I_MapMaker mapMaker = target as I_MapMaker;
+        if (GUILayout.Button("マップ生成"))
+        {
+            mapMaker.Make();
+        }
+        if (GUILayout.Button("子オブジェクト全削除"))
+        {
+            mapMaker.DeleteChildren();
+        }
+    }
+}
