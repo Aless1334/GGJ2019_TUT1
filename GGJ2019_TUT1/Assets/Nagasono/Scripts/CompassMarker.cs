56c867d74ca8b94bed39cb9d29ca98f2a9172b1c
Aless1334 <aless3511@gmail.com>
2019-01-27 00:11:40 +0900

    コンパス機能作成

---
+++ GGJ2019_TUT1/Assets/Nagasono/Scripts/CompassMarker.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompassMarker : MonoBehaviour
+{   
+    [SerializeField] private Goal goalObject;
+    [SerializeField] private Transform baseTransform;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var direction = (goalObject.transform.position - baseTransform.position).normalized;
+        var lookRotation = Quaternion.LookRotation(Vector3.back, direction);
+        transform.localRotation = lookRotation;
+    }
+}
