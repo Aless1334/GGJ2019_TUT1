8652db9462b126a2d221de27e8eadd651b5ac242
Aless1334 <aless3511@gmail.com>
2019-01-27 09:12:18 +0900

    ライトに狼指示効果を追加

--- GGJ2019_TUT1/Assets/Nagasono/Scripts/LightMask.cs
+++ GGJ2019_TUT1/Assets/Nagasono/Scripts/LightMask.cs
@@ -6,6 +6,18 @@ public class LightMask : MonoBehaviour
 {
     public float ariveTime { private get; set; }
 
+    private void OnEnable()
+    {
+        var colliders = Physics2D.OverlapCircleAll(transform.position, transform.localScale.x / 2.0f);
+
+        Wolf_ver3 wolf;
+        foreach (var collider1 in colliders)
+        {
+            if ((wolf = collider1.GetComponent<Wolf_ver3>()) == null) continue;
+            wolf.StartChase();
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
