9c032d64687c5a1fc760b3b7741f42614894733b
IchinoseKoki <namefree.ichi@icloud.com>
2019-01-27 11:04:12 +0900

    fade

--- GGJ2019_TUT1/Assets/Ichinose/Scripts/Scene/FadeManager.cs
+++ GGJ2019_TUT1/Assets/Ichinose/Scripts/Scene/FadeManager.cs
@@ -76,7 +76,7 @@ public class FadeManager : MonoBehaviour
     {
         if (GetFadeEnd() && !manualControl)
         {
-            Load();
+            SceneLoad();
             return;
         }
 
